bef8ef72e909967de4404ced5ae8103af64c7f89
hayakawa <hayakawatakahiko@gmail.com>
2018-09-05 22:09:04 +0900

    projectのimport

---
+++ src/main/java/com/github/hayataka/prepTest/App.java
@@ -0,0 +1,13 @@
+package com.github.hayataka.prepTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
